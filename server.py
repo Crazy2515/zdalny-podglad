546ced7289a4b029813ef043db788aa7c8fb57ec
Crazy2515 <piotr555380@gmail.com>
2025-04-08 17:30:15 +0200

    Update server.py

--- server.py
+++ server.py
@@ -23,4 +23,6 @@ def view_screenshot():
     return send_file(filepath, mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
